25123128e309731f86a92f96df76e70fb953b15a
RyabovNA <kulek19862009@mail.ru>
2024-08-03 12:14:07 +0500

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,13 +1,12 @@
 my_dict = {'Daniil': 2012, 'Vasiliy': 2017, 'Nikolay': 1986}
-print(my_dict)
-print(my_dict['Daniil'])
-my_dict['Elena'] = 1988
-print(my_dict['Elena'])
+print('Dict:', my_dict)
+print('Existing value:', my_dict['Daniil'])
+print('Not existing value:', my_dict.get('Elena'))
 my_dict.update({'Kseniya': 2001,
                 'Alexsandr': 1968})
 Vasiliy = my_dict.pop('Vasiliy')
-print(Vasiliy)
-print(my_dict)
+print('Deleted value:', Vasiliy)
+print('Modified dictionary:', my_dict)
 
 my_set = {4, 5, 6, 6, 4, 'Vika', 'Sonya', 'Vika', 1, 5}
 print('Set:', my_set)
